fe64a8de4b543377798f310222f9dc30061253a3
leva <freedomlove6574@gmail.com>
2026-02-16 21:29:12 +0300

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/com/example/mykmp/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.example.mykmp
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
